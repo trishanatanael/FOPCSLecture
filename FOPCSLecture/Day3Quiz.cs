0cdf9596bc68b9a380c7a88d6fe0c1ba3dd5ee05
t <trisha.natanael@gmail.com>
2017-02-09 13:28:47 +0800

    All quiz added. Quiz 4 needs to be finished

---
+++ FOPCSLecture/Day3Quiz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOPCSLecture
+{
+    using System;
+    class Day3Quiz
+    {
+        public static void Main(string[] args)
+        {
+          
+        }
+    }
+}
---
+++ FOPCSLecture/Day4Quiz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOPCSLecture
+{
+    using System;
+    class Day4Quiz
+    {
+        public static void Main(string[] args)
+        {
+            bool wrongPIN = true;
+            int numTries = 0;
+            string PIN = "123456";
+
+            Console.WriteLine("Welcome to the Bank of ISS");
+            Console.Write("Enter your PIN:");
+
+            while (wrongPIN && numTries < 3)
+            {
+                Console.Write("Enter your PIN:");
+                string input = Console.ReadLine();
+                numTries++;
+
+                if (input == PIN)
+                {
+                    Console.WriteLine("PIN accepted. You can access your account now");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect PIN");
+                    if (wrongPIN)
+                    {
+                    }
+                }
+            }
+
+        }
+    }
+}
